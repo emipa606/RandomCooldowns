599cdfb1ed3b2c26f79aef347b5d03dfce309e33
Mlie <emipa606@users.noreply.github.com>
2022-01-21 09:02:53 +0100

    Should now work with animals and other pawns that have custom ranged attacks not based on shooting skill.

--- Source/RandomizedCooldowns/Patch.cs
+++ Source/RandomizedCooldowns/Patch.cs
@@ -24,34 +24,38 @@ internal class Patch
     {
         // A lot of things that aren't ranged attacks use TryCastNextBurstShot.
         // Also, don't alter cooldown between burst shots.
-        if (!__instance.CasterIsPawn || __instance.verbProps.nonInterruptingSelfCast ||
-            !__instance.verbProps.LaunchesProjectile || __instance.Bursting)
+        if (!__instance.CasterIsPawn || __instance.verbProps?.nonInterruptingSelfCast == true ||
+            !__instance.verbProps?.LaunchesProjectile == true || __instance.Bursting)
         {
             return;
         }
 
         // Don't modify extremely short cooldowns.
-        if (__instance.CasterPawn.stances.curStance is not Stance_Cooldown stance || stance.ticksLeft <= 29)
+        if (__instance.CasterPawn?.stances?.curStance is not Stance_Cooldown stance || stance.ticksLeft <= 29)
         {
             return;
         }
 
         var range = new FloatRange(0, 1f);
         // Take into account skillcap removers
-        var skill = Math.Min(__instance.CasterPawn.skills.GetSkill(SkillDefOf.Shooting).levelInt, 20);
-        float skillpotency = 0;
-        switch (skill)
+        var levelInt = __instance.CasterPawn?.skills?.GetSkill(SkillDefOf.Shooting).levelInt;
+        if (levelInt != null)
         {
-            case < 10:
-                // If below 10 skill, add lower possible cooldown descrease in the randomizer
-                skillpotency = 1f * (skill / 10f);
-                range.min = Midpoint - (Midpoint * skillpotency);
-                break;
-            case > 10:
-                // If above 10 skill, add lower possible cooldown increase in the randomizer
-                skillpotency = 1f * ((skill - 10) / 10f);
-                range.max = Highpoint - ((Highpoint - Midpoint) * skillpotency);
-                break;
+            var skill = Math.Min((float)levelInt, 20);
+            float skillpotency;
+            switch (skill)
+            {
+                case < 10:
+                    // If below 10 skill, add lower possible cooldown descrease in the randomizer
+                    skillpotency = 1f * (skill / 10f);
+                    range.min = Midpoint - (Midpoint * skillpotency);
+                    break;
+                case > 10:
+                    // If above 10 skill, add lower possible cooldown increase in the randomizer
+                    skillpotency = 1f * ((skill - 10) / 10f);
+                    range.max = Highpoint - ((Highpoint - Midpoint) * skillpotency);
+                    break;
+            }
         }
 
         var random_ticks = (int)RandomCooldownTicksCurve.Evaluate(Rand.Range(range.min, range.max));
